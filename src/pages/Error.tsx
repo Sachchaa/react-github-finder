dffd4adb3fc8bf01da28137ac705728898c0bdf6
Sachin Kanishka <sachinkanishka001@gmail.com>
2021-07-19 23:20:14 +0930

    #day05 -
    created error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
     return (
         <div>
-            <h2>Hello! This is Error page</h2>
+            <h1>404</h1>
+            <h3>Sorry, The Page You Tried Cannot Be Found</h3>
+            <Link to="/">Back Home</Link>
         </div>
     )
 }
